38fcb8ac055386e664fe3ace245b929dd2fd5d2d
Phildo <Phildo211@gmail.com>
2013-11-06 20:47:03 -0600

    initial commit

---
+++ talk.js
@@ -0,0 +1,21 @@
+/*
+* Javascript
+* A thing where phil talks about it
+*/
+
+var dude = {
+  "name"      :"Phil",
+  "age"       :23,
+  "experience":"not that much"
+  };
+var language = "Javascript";
+var version = 23;
+
+var intro = 
+"Yo- I'm "+dude.name+". 
+"there";
+
+function intro()
+{
+  
+}
